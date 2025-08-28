15c1f1a8093f6a9c5d7971080e0e7570dc3ad7e0
loic-job-dev <loicjobard@hotmail.com>
2025-08-28 16:47:08 +0200

    add some javadoc

--- src/fr/campus/loic/agameofdragons/material/Cell.java
+++ src/fr/campus/loic/agameofdragons/material/Cell.java
@@ -3,13 +3,26 @@ package fr.campus.loic.agameofdragons.material;
 import fr.campus.loic.agameofdragons.Menu;
 import fr.campus.loic.agameofdragons.characters.Character;
 
+/**
+ * This class represents a tile on the board
+ */
 public abstract class Cell {
     protected boolean isAlreadyVisited;
 
+
+    /**
+     * The constructor sets a boolean that is usefull to indicate if the tile has been visited
+     */
     public Cell () {
         this.isAlreadyVisited = false;
     }
 
+    /**
+     * This method describes the consequence of the arrival of the player on this tile
+     *
+     * @param character is the character of the player
+     * @param menu is the menu created to display messages
+     */
     public void action (Character character, Menu menu){
 
     }
